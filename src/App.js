66f39dc45841b0fdee9de91100a52352f586dc2f
headphoenix <fiifi.phoenix@gmail.com>
2022-11-03 12:30:59 +0000

    Added Bag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { CreateContainer, Header, MainContainer, Product } from "./components";
+import { CreateContainer, Header, MainContainer, Product, Cart } from "./components";
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
 import { actionType } from "./context/reducer";
@@ -32,6 +32,7 @@ const App = () => {
             <Route path="/*" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
             <Route path="/product" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </main>
       </div>
